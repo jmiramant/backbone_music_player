695d9c9d3432e4d3c0bd8601905da3c2c933b426
jmiramant <josh@miramant.me>
2013-08-29 18:22:23 -0700

    updated template

---
+++ backbone_tunes/public/js/Tunes.js
@@ -0,0 +1,33 @@
+(function($) {
+
+	window.Album = Backbone.Model.extend({
+
+		isFirstTrack: function(index){
+			return index == 0;
+		}
+
+	});
+
+	window.AlbumView = Backbone.View.extend({
+		tagName: 'li',
+		className: 'album',
+		initialize: function(){
+			this.template = _.template($('#album-template').html());
+		},
+		render: function(){
+			_.bindAll(this, 'render');
+			this.model.bind('change', this.render);
+
+			var renderContent = this.template(this.model.toJSON());
+			$(this.el).html(renderContent);
+			return this;
+			}	
+	});
+
+
+})(jQuery);
+
+
+// album = new Album({title: 'Mumford and Sons', artist: 'Mumford and Sons', tracks:[{title: "Little Mountian Man"},{title: 'I will wait'}]})
+// albumview = new AlbumView({model:album})
+// $('#container').append(albumview.render().el)
